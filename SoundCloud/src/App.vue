ba956a2dbc1eadecea66a018762bd1a5655176de
thaitd <duythai010305@gmail.com>
2024-03-08 13:46:05 +0700

    push

---
+++ SoundCloud/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+ <div class="container">
+Thasi is the SoundCloud
+ </div>
+</template>
+
+<style scoped>
+</style>
